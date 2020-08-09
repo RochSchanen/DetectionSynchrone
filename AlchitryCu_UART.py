0a332299067f4decb0f429f14138af9ea16a2778
RochSchanen <r.schanen@lancaster.ac.uk>
2020-08-09 21:52:39 +0100

    add config. display of the com port, add load 8 bits loading to 8 leds, partially working.

--- AlchitryCu_UART.py
+++ AlchitryCu_UART.py
@@ -47,37 +47,92 @@
 #          Please install linux-gpib (Linux) or gpib-ctypes (Windows, Linux) to use this resource type. Note that installing gpib-ctypes will give you access to a broader range of funcionality.
 #          No module named 'gpib'
 
-# python3 code:
+# intrument name found the local machine
+# >python3
+# >>>import pyvisa
+# >>>rm = pyvisa.ResourceManager()
+# >>>print(rm.list_resources())
+# prints the available intruments name
+# (	'ASRL/dev/ttyS4::INSTR', 
+# 	'ASRL/dev/ttyS0::INSTR',
+# 	'ASRL/dev/ttyUSB1::INSTR',
+# 	'ASRL/dev/ttyUSB0::INSTR'
+# )
+# ttyUSB0 is used to program the Alchitry Cu EEPROM
+# ttyUSB1 is used for communicating with the configured FPGA
 
-import pyvisa
-rm = pyvisa.ResourceManager()
-
-# print(rm.list_resources())
-# devices available
-# ('ASRL/dev/ttyS4::INSTR', 'ASRL/dev/ttyS0::INSTR', 'ASRL/dev/ttyUSB1::INSTR')
-
-AlCu_UART = rm.open_resource('ASRL/dev/ttyUSB1::INSTR')
-
-AlCu_UART.write("hello 1")
-AlCu_UART.write("hello 2")
-AlCu_UART.write("hello 3")
-AlCu_UART.write("hello 4")
-
-print(f"|{AlCu_UART.read()}|")
-print(f"|{AlCu_UART.read()}|")
-print(f"|{AlCu_UART.read()}|")
-print(f"|{AlCu_UART.read()}|")
-
-AlCu_UART.close()
-
+# make executable
 # >chmod u+x AlchitryCu_UART.py
+
+# usage:
 # >./AlchitryCu_UART.py
 
-# |hello 1
-# |
-# |hello 2
-# |
-# |hello 3
-# |
-# |hello 4
-# |
+PORTNAME = 'ASRL/dev/ttyUSB1::INSTR'
+
+#################################################
+# import useful constants and methods from pyvisa
+
+from pyvisa.constants import VI_ASRL_STOP_ONE
+from pyvisa.constants import VI_ASRL_STOP_ONE5
+from pyvisa.constants import VI_ASRL_STOP_TWO
+stop_bits_constants = {
+	VI_ASRL_STOP_ONE 	:	"1.0",
+	VI_ASRL_STOP_ONE5 	:	"1.5",
+	VI_ASRL_STOP_TWO 	:	"2.0"
+}
+
+from pyvisa.constants import VI_ASRL_END_BREAK
+from pyvisa.constants import VI_ASRL_END_NONE
+from pyvisa.constants import VI_ASRL_END_TERMCHAR
+from pyvisa.constants import VI_ASRL_END_LAST_BIT
+end_input_constant = {
+	VI_ASRL_END_BREAK 		: "Break",
+	VI_ASRL_END_NONE 		: "None",
+	VI_ASRL_END_TERMCHAR	: "TermChar",
+	VI_ASRL_END_LAST_BIT	: "LastBit"
+}
+
+from pyvisa.constants import VI_ASRL_PAR_EVEN
+from pyvisa.constants import VI_ASRL_PAR_MARK 
+from pyvisa.constants import VI_ASRL_PAR_NONE
+from pyvisa.constants import VI_ASRL_PAR_ODD
+from pyvisa.constants import VI_ASRL_PAR_SPACE
+parity_constants = {
+	VI_ASRL_PAR_EVEN 	: "Even",
+	VI_ASRL_PAR_MARK 	: "Mark",
+	VI_ASRL_PAR_NONE 	: "None",
+	VI_ASRL_PAR_ODD 	: "Odd",
+	VI_ASRL_PAR_SPACE 	: "Space"
+}
+
+from pyvisa import ResourceManager
+
+# import useful constants and methods from pyvisa
+#################################################
+
+import sys
+
+print("file: AlchitryCu_UART.py")
+print("content: Alchitry Cu UART tester")
+print("created: 2020 August 8 Saturday")
+print("author: roch schanen")
+print("comment: test UART communication with the FPGA")
+print("run Python3:" + sys.version);
+
+# instanciate the ResourceManager
+rm = ResourceManager()
+
+# instanciate the instrument
+if PORTNAME in rm.list_resources():
+	print(f"Open port {PORTNAME}")
+	AlCu_UART = rm.open_resource(PORTNAME)
+	AlCu_UART.baud_rate = 781250 # 100MHz / 2^7
+	print(f"Baud rate = {AlCu_UART.baud_rate} bit/s")
+	print(f"Data bits = {AlCu_UART.data_bits} bits")
+	print(f"Stop bits = {stop_bits_constants[AlCu_UART.stop_bits]} bit(s)")
+	print(f"Parity    = {parity_constants[AlCu_UART.parity]}")
+
+AlCu_UART.write("Z")
+print(f"-{AlCu_UART.read()}-")
+
+AlCu_UART.close()
--- AlchitryCu_UART.vhd
+++ AlchitryCu_UART.vhd
@@ -2,47 +2,256 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
-
 entity AlchitryCu_UART is
 	port(
 		AlCu_CLOCK : in  std_logic;		-- clock 100MHz
 		AlCu_LED_0 : out std_logic; 	-- output LED
 		AlCu_LED_1 : out std_logic; 	-- output LED
+		AlCu_LED_2 : out std_logic; 	-- output LED
+		AlCu_LED_3 : out std_logic; 	-- output LED
+		AlCu_LED_4 : out std_logic; 	-- output LED
+		AlCu_LED_5 : out std_logic; 	-- output LED
+		AlCu_LED_6 : out std_logic; 	-- output LED
+		AlCu_LED_7 : out std_logic; 	-- output LED
 		AlCu_UART_RX : in  std_logic;	-- UART read
 		AlCu_UART_TX : out std_logic  	-- UART transmit
 	);
 end entity AlchitryCu_UART;
 
+-- todo:
+-- todo: maybe only clear the MSB of the counter
+-- todo:
+
 architecture AlchitryCu_UART_arch of AlchitryCu_UART is
 
-	-- counter
-	signal counter : std_logic_vector(25 downto 0);
+	-- Finite State Machine Declarations
+	-- (tState is the states types : enumerated)
+	-- (vState is the state value, default is "sReady")
+	type   tState is (sReady, sStart, sReceive, sStop, sValidate);
+	signal vState : tState := sReady;
+
+	-- data registers
+	signal data : std_logic_vector (7 downto 0) := (others => '0');
+	signal btpt : integer range 0 to 7 := 0; -- bit pointer
+	--signal dval : std_logic := '0'; -- data validate
+
+	-- counter 
+	-- (128 cycles is one UART pulse width: check bit 7)
+	-- (64 cycles is half a UART pulse width: check bit 6)
+	-- serial port baud rate must be set at 781250 bit/s
+	-- This gives 781250*128 = 100 000 000 Hz = clock freq.
+	signal cntr : std_logic_vector(7 downto 0);
 
 	begin
 
-	-- connect RX to TX
-	-- this echo any input straight to the output
-	-- useful for testing USB port communication
-	AlCu_UART_TX <= AlCu_UART_RX;
+		-- echo received data to transmit data for test
+		AlCu_UART_TX <= AlCu_UART_RX;
 
-	-- connect the transmit signal to a LED for checking transmission
-	AlCu_LED_1 <= AlCu_UART_RX;
+		pUART : process (AlCu_CLOCK)
 
-	-- we keep the blink part to check the clock
-	-- both part are independent
+			begin
 
-	-- increment on clock rising edge
-	process (AlCu_CLOCK)
-	begin
-		if rising_edge(AlCu_CLOCK) then
-			-- conversions and increment counter
-			counter <= std_logic_vector(unsigned(counter)+1);
-		end if;
-	end process;
+				-- update at rising edges of the clock
+				if rising_edge (AlCu_CLOCK) then
 
-	-- output most significant bit of the counter
-	AlCu_LED_0 <= counter(25);
-	
-	-- done
+					case vState is
+					
+
+						-- READY
+						-------------------------------------------------------
+						-- the machine starts in the waiting state "Ready"
+						-- it waits for the received level to go low
+
+						when sReady =>
+
+							-- level is low: the machine goes into the "Start" state
+							if AlCu_UART_RX = '0' then
+
+								-- go to the "Start" state
+								vState <= sStart;
+							
+							-- level is high: keeps the machine in the waiting state
+							else
+
+								-- keep the machine in the "Ready" state
+								vState <= sReady;
+
+							end if;
+							
+							-- in all circumstances, counter and
+							-- bit pointer a set to zero:
+							
+							-- clear counter
+							cntr <= (others => '0');
+
+							-- clear data validate
+							--dval <= '0';
+
+							-- clear bit pointer (is this in the correct place?)                 !
+							btpt <= 0; 				 
+				
+
+
+						-- START
+						-------------------------------------------------------
+						-- wait for half a period and confirm the low level value
+						
+						when sStart =>
+							
+							-- check if half period is reached (bit 6 == 64 clock cycles)
+							if cntr(6) = '1' then
+								
+								-- check "Receive" level
+								if AlCu_UART_RX = '0' then
+									
+									-- clear counter
+									cntr <= (others => '0');
+
+									-- go to the "Receive" state
+									vState <= sReceive;
+
+								-- clear bit pointer (is this in the correct place?)             !
+								--btpt <= 0; 
+								
+								else
+									-- if the level is high, assume that the initial
+									-- low level value was a spurious signal. go back
+									-- to the "Ready" state.
+									vState <= sReady;
+								
+								end if;
+
+							-- wait until half period
+							else
+								
+								-- increment counter
+								cntr <= std_logic_vector(unsigned(cntr)+1);
+								
+								-- keep the machine in the "Start" state 
+								vState <= sStart;
+							
+							end if;
+
+
+						-- RECEIVE
+						-------------------------------------------------------
+						-- wait for a full period and read the data level
+						
+						when sReceive =>
+							
+							-- check if a full period is reached (bit 7 == 128 clock cycles)
+							if cntr(7) = '1' then
+								
+								-- clear counter
+								cntr <= (others => '0'); 
+
+								-- record the "Receive" value and increase bit pointer
+								data(btpt) <= AlCu_UART_RX;
+								
+								-- last data bit
+								if btpt = 7 then
+
+									-- clear bit pointer (is this in the correct place?)         !
+									btpt <= 0;
+									
+									-- go to "Stop" state
+									vState <= sStop;
+
+								-- not the last data bit
+								else
+
+									-- increment bit pointer
+									btpt <= btpt + 1;
+
+									-- continue in "Receive" state
+									vState <= sReceive;
+
+								end if;
+
+							-- continue with increasing the counter
+							else
+
+								-- increment counter
+								cntr <= std_logic_vector(unsigned(cntr)+1);
+
+								-- continue in "Receive" state
+								vState <= sReceive;
+
+							end if;
+
+
+						-- STOP
+						-------------------------------------------------------
+						-- wait for another full period and set the validate signal
+
+						when sStop =>
+
+							if cntr(7) = '1' then
+
+								-- clear counter
+								cntr <= (others => '0'); 
+
+								-- set validate signal
+								--dval <= '1';
+
+								-- go to "Validate" state
+								vState <= sValidate;
+
+							else
+
+								-- increment counter
+								cntr <= std_logic_vector(unsigned(cntr)+1);
+
+								-- continue in "Stop" state
+								vState <= sStop;
+
+							end if;
+
+
+						-- VALIDATE
+						-------------------------------------------------------
+						-- the validate pulse last for only one AlCu_CLOCK cycle.
+						-- it is triggered at the middle of the UART stop pulse.
+						-- it is ended in the "Validate" state immediately.
+
+						when sValidate =>
+
+							-- clear validate
+							--dval <= '0';
+
+							-- go to "Ready" state
+							vState <= sReady;
+
+
+						-- OTHERS
+						-------------------------------------------------------
+						-- all other cases
+
+						when others =>
+
+							-- in any other case, go back to the "Ready" state
+							vState <= sReady;
+
+
+					-- done
+					end case;
+
+				-- end AlCu_CLOCK rising_edge event
+				end if;
+
+		-- todo: set the leds
+
+
+		end process pUART;
+
+	AlCu_LED_0 <= data(0);
+	AlCu_LED_1 <= data(1);
+	AlCu_LED_2 <= data(2);
+	AlCu_LED_3 <= data(3);
+	AlCu_LED_4 <= data(4);
+	AlCu_LED_5 <= data(5);
+	AlCu_LED_6 <= data(6);
+	AlCu_LED_7 <= data(7);
+
+-- done
 end architecture AlchitryCu_UART_arch;
