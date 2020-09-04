803afaa1d524b72635097a21f3a1c2bb6a555cd1
RochSchanen <r.schanen@lancaster.ac.uk>
2020-09-04 21:14:53 +0100

    further test: see log

--- AlchitryCu.vhd
+++ AlchitryCu.vhd
@@ -22,13 +22,21 @@ use work.components.all;
 -------------------------------------------------
 
 entity AlchitryCu is
-    port(AlBr_IO_32 : out   std_logic;  -- Bank B 2
-         AlBr_IO_33 : out   std_logic;  -- Bank B 5
-         AlBr_IO_34 : out   std_logic;  -- Bank B 8
-         AlBr_IO_35 : out   std_logic;  -- Bank B 11
-         AlBr_IO_36 : out   std_logic;  -- Bank B 14
-         AlBr_IO_37 : out   std_logic;  -- Bank B 17
-         AlBr_IO_38 : out   std_logic;  -- Bank B 20
+    port(AlBr_IO_08 : out   std_logic;  -- Bank B 2
+         AlBr_IO_09 : out   std_logic;  -- Bank B 5
+         AlBr_IO_10 : out   std_logic;  -- Bank B 8
+         AlBr_IO_11 : out   std_logic;  -- Bank B 11
+         AlBr_IO_12 : out   std_logic;  -- Bank B 14
+         AlBr_IO_13 : out   std_logic;  -- Bank B 17
+         AlBr_IO_14 : out   std_logic;  -- Bank B 20
+         AlBr_IO_15 : out   std_logic;  -- Bank B 23
+         AlBr_IO_32 : out   std_logic;  -- Bank B 23
+         AlBr_IO_33 : out   std_logic;  -- Bank B 23
+         AlBr_IO_34 : out   std_logic;  -- Bank B 23
+         AlBr_IO_35 : out   std_logic;  -- Bank B 23
+         AlBr_IO_36 : out   std_logic;  -- Bank B 23
+         AlBr_IO_37 : out   std_logic;  -- Bank B 23
+         AlBr_IO_38 : out   std_logic;  -- Bank B 23
          AlBr_IO_39 : out   std_logic;  -- Bank B 23
          AlCu_CLOCK : in    std_logic); -- 100MHz clock
 end entity AlchitryCu;
@@ -52,15 +60,15 @@ begin
     -- instanciate clock divider
 
     clkdiv1 : clkdiv
-        --generic map(10)              -- division by 1024
-        generic map(2)                 -- division by 2
+        generic map(5)             -- division by 1024
+        --generic map(2)             -- division by 2
         port map(AlCu_CLOCK, clk);
 
     -- instanciate accumulator
 
     plnco1 : plnco
         generic map(ACCSIZE, CNTSIZE)
-        port map('1', clk, "1011", acc, c);
+        port map('1', clk, "0001", acc, c);
 
     -- instanciate srom
 
@@ -69,15 +77,17 @@ begin
 
     ---- output value
 
-    AlBr_IO_32 <= o(7);
-    AlBr_IO_33 <= o(6);
-    AlBr_IO_34 <= o(5);
-    AlBr_IO_35 <= o(4);
-    AlBr_IO_36 <= o(3);
-    AlBr_IO_37 <= o(2);
-    AlBr_IO_38 <= o(1);
-    AlBr_IO_39 <= o(0);
+    AlBr_IO_08 <= o(7);
+    AlBr_IO_09 <= o(6);
+    AlBr_IO_10 <= o(5);
+    AlBr_IO_11 <= o(4);
+    AlBr_IO_12 <= o(3);
+    AlBr_IO_13 <= o(2);
+    AlBr_IO_14 <= o(1);
+    AlBr_IO_15 <= o(0);
 
+    AlBr_IO_32 <= clk;
+ 
 -- done
 end architecture arch;
 
